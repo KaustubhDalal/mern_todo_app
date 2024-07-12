17a574a2b3c8072a692f1347429386e2e9bf9e29
KaustubhDalal <kaustubhvd5@gmail.com>
2024-07-12 10:07:31 +0530

    Added mango db connection

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 //init app
 const app = express();
 
 const PORT = 8000;
 
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+mongoose
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("Connected to todoDb database");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 //view engine
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
